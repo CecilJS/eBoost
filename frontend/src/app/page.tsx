9aa6402bcba50bef8e20500a59e356deb8fc18c4
Cecil Aboagye-Nyarko <cecil.developer@outlook.com>
2023-07-11 23:05:01 +0100

    add landing page prototype

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,26 @@
-import Image from "next/image";
+import './globals.css'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 bg-gradient-to-b from-zinc-200 bg-[url('C:\Users\Cecil\desktop\eBoost\frontend\src\app\assets\images\man_made_easy.jpeg')]">
-      <div className="container mx-auto">
-        <p className="fixed left-0 top-0 flex justify-left border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    <main>
+      <nav className="container mx-auto">
+        <a className="fixed left-0 top-0 flex justify-left nav-text">
           eBoost
-        </p>
-      </div>
+        </a>
+      </nav>
+      <section className="cta-landing-page">
+        <h1>Maintainence Made Easy</h1>
+        <div className='cta-btns'>
+          <div className='cta-btn'>Predictions</div>
+          <div className='cta-btn'>Logs</div>
+        </div>
+
+      </section>
+      <footer>
+        <a className="fixed center-0 bottom-0 flex nav-text">
+          &copy; eBoost - {new Date().getFullYear()}
+        </a>
+      </footer>
     </main>
   );
 }
