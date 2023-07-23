acc0e6b21a1e5d809956b2b9b993291221dbe8db
Cecil Jones <dewdrops1800@outlook.com>
2023-07-24 00:26:09 +0100

    style landing page

--- frontend/src/app/Navbar.tsx
+++ frontend/src/app/Navbar.tsx
@@ -4,14 +4,14 @@ export default function Navbar() {
   return (
     <nav className="container flex flex-row justify-between mx-auto px-5 py-5">
       <div>
-        <a>eBoost</a>
+        <a className="text-eBoost-blue font-bold text-1xl">eBoost</a>
       </div>
       <div className="flex items-end">
-        <ul className="flex flex-row list-none justify-evenly">
+        <ul className="flex flex-row list-none justify-evenly space-x-8 cursor-pointer px-8 py-2 font-semibold text-eBoost-blue">
           <li>Home</li>
           <li>About</li>
           <li>Messages</li>
-          <li>Maintenance Schedule</li>
+          <li>Maintenance </li>
           <li>Login</li>
           <li>Settings</li>
         </ul>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,17 +3,26 @@ import Navbar from "./Navbar";
 
 export default function Home() {
   return (
-    <main>
+    <main className="font-sans">
       <Navbar />
-      <section className="container flex flex-col justify-center my-10">
-        <h1 className="text-2xl font-bold py-15">Maintainence Made Easy</h1>
-        <div className="items-center">
-          <div className="text-blue-600/100">Predictions</div>
-          <div className="text-blue-600/75">Logs</div>
+      <section className="flex h-screen items-center justify-center">
+        <div>
+          <h1 className="text-6xl font-bold text-eBoost-blue shadow-xl py-4 px-4">
+            Maintainence Made Easy
+          </h1>
+          <hr />
+          <div className="flex flex-row items-center justify-center my-10 justify-evenly mt-8">
+            <div className="text-1xl font-bold text-eBoost-blue cursor-pointer rounded-xl bg-white-300 p-4 border border-eBoost-blue w-64 text-center hover:bg-eBoost-blue hover:text-white">
+              Predictions
+            </div>
+            <div className="text-1xl font-bold text-white cursor-pointer rounded-xl bg-eBoost-blue p-4 border w-64 text-center hover:bg-white hover:text-eBoost-blue hover:border-eBoost-blue">
+              Logs
+            </div>
+          </div>
         </div>
       </section>
-      <footer>
-        <a className="fixed center-0 bottom-0 flex">
+      <footer className="flex">
+        <a className="flex fixed bottom-0 flex items-center justify-center">
           &copy; eBoost - {new Date().getFullYear()}
         </a>
       </footer>
