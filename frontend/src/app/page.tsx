043bf339bc070fcfbca1e984206c68bfcefccd6e
Cecil Jones <dewdrops1800@outlook.com>
2023-07-26 00:26:57 +0100

    add predictions and logs pages

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,43 @@
 import "./globals.css";
-import Navbar from "./Navbar";
+import Navbar from "../components/Navbar";
+import Button from "../components/Button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="font-sans">
       <Navbar />
-      <section className="flex h-screen items-center justify-center">
+      <section className="flex h-screen items-center justify-center mb-0">
         <div>
           <h1 className="text-6xl font-bold text-eBoost-blue shadow-xl py-4 px-4">
             Maintainence Made Easy
           </h1>
           <hr />
           <div className="flex flex-row items-center justify-center my-10 justify-evenly mt-8">
-            <div className="text-1xl font-bold text-eBoost-blue cursor-pointer rounded-xl bg-white-300 p-4 border border-eBoost-blue w-64 text-center hover:bg-eBoost-blue hover:text-white">
-              Predictions
-            </div>
-            <div className="text-1xl font-bold text-white cursor-pointer rounded-xl bg-eBoost-blue p-4 border w-64 text-center hover:bg-white hover:text-eBoost-blue hover:border-eBoost-blue">
-              Logs
-            </div>
+            <Link href="/predictions">
+              <Button
+                textColor="text-eBoost-blue"
+                bgColor="bg-white-300"
+                borderColor="border-eBoost-blue"
+                hoverTextColor="text-white"
+                hoverBgColor="bg-eBoost-blue"
+                hoverBorderColor="border-eBoost-blue"
+              >
+                Predictions
+              </Button>
+            </Link>
+            <Link href="/logs">
+              <Button
+                textColor="text-white"
+                bgColor="bg-eBoost-blue"
+                borderColor="border-eBoost-blue"
+                hoverTextColor="text-eBoost-blue"
+                hoverBgColor="bg-white"
+                hoverBorderColor="border-eBoost-blue"
+              >
+                Logs
+              </Button>
+            </Link>
           </div>
         </div>
       </section>
