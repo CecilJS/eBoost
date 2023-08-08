8f15f91f07e1993a306443cf3dc39fdea26f8df1
Cecil Jones <dewdrops1800@outlook.com>
2023-08-08 02:10:50 +0100

    add unit tests

---
+++ frontend/src/app/__Tests__/Button.spec.js
@@ -0,0 +1,6 @@
+/***
+ * Write unit tests to assert that the components render as expected
+ *
+ * Consider writing some cypress tests to verify the integration of various components
+ *
+ */
---
+++ frontend/src/services/logs/logService.js
@@ -0,0 +1,10 @@
+/***
+ * Write a service that fetches data from the API
+ * and returns the response to be displayed on the
+ * frontend
+ *
+ * Write tests to verify the service behaves as expected
+ *
+ * Import this service in the right component and store the response in a local state to be
+ * conditionally rendered in the UI
+ */
